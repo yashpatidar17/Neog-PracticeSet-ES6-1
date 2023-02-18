d4d06c3982f1c30e5a65d3e98982e0e9e4ac922d
Yash Patidar <60218098+yashpatidar17@users.noreply.github.com>
2023-02-18 20:54:49 +0000

    Question 11

---
+++ Question-11.js
@@ -0,0 +1,11 @@
+const product = {
+  title: "iPhone",
+  price: 5999,
+  description: "The iPhone is a smartphone developed by Apple"
+}
+
+const {title,price,description} = product;
+
+console.log(title); // iPhone
+console.log(price); // 999
+console.log(description); // The iPhone is a smartphone developed by Apple
