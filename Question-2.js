2215c22d5273d0bb0586f73c68e5237f348dbd98
Yash Patidar <60218098+yashpatidar17@users.noreply.github.com>
2023-02-18 19:50:27 +0000

    Question 2

---
+++ Question-2.js
@@ -0,0 +1,10 @@
+const isGreater = (a, b) => {
+  if (a > b) {
+    return "a is greator than b"
+  } else {
+    return "b is greator than a"
+  }
+}
+
+console.log(isGreater(2, 5)) // 5 is greater than 2
+console.log(isGreater(10, 5)) // 10 is greater than 5
