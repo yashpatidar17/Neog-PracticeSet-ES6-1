2aa7ce7c017bd9138ad1b3313fc7ebb70d543cb7
Yash Patidar <60218098+yashpatidar17@users.noreply.github.com>
2023-02-19 14:58:35 +0000

    Question 14

---
+++ Question-14.js
@@ -0,0 +1,6 @@
+//Given an array numbers containing the numbers 1, 2, and 3. Use array destructuring to log each number to the console.
+
+const number = [1,2,3,4];
+const[a,b,c,d] = number;
+
+console.log(c);
